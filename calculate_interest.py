b539e2eb67c13d01c99c57ae7527354d615ee9ca
Sudharsan410 <138292436+Sudharsan410@users.noreply.github.com>
2025-06-20 15:04:52 +0530

    Update calculate_interest.py

--- calculate_interest.py
+++ calculate_interest.py
@@ -1,3 +1,7 @@
-# Simple Interest Calculator
-def calculate_simple_interest(principal, rate, time):
-    return (principal * rate * time) / 100
+A calculator that calculates simple interest given principal, annual rate of interest and time period in years.
+Input:
+   p, principal amount
+   t, time period in years
+   r, annual rate of interest
+Output
+   simple interest = p*t*r
